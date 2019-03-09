78aab6b46d6ee0861f82b1e9af464a4a7080a351
plcexpert <39923158+plcexpert@users.noreply.github.com>
2019-03-09 13:33:55 +0100

    Revert back

---
+++ PlcLab_Poti/PlcLab_Poti.ino
@@ -0,0 +1,30 @@
+/******************************************************************
+  Project name : Plc Lab - Potentiometer
+  Author       : PLC-EXPERT
+  Date Time    : 2019-01-02
+  Description  : Potentiometer example
+  CPU BOARD    : Arduino NANO
+  URL          : www.plc-expert.com
+******************************************************************/
+
+int analogPin = A6;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  // Read analog value
+  int analogValue = analogRead(analogPin);
+
+  Serial.print("Analog Value = ");
+  Serial.print(analogValue);
+
+  // Percentage
+  int percentValue = map(analogValue, 0, 1023, 0, 100)
+
+  ()*100L/(1023);
+
+  Serial.print("       Analog percentage = ");
+  Serial.println(percentValue);
+}
